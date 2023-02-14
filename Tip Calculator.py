992b9ed89838555dae34b58cff949e19c13e0c7c
Sangg-27 <116178099+Sangg-27@users.noreply.github.com>
2023-02-14 18:42:03 +0530

    Update and rename Band Name Generator.py to Tip Calculator.py

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,11 +1,10 @@
+#Day 1 TIP CALCULATOR
+
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
 
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
@@ -17,3 +16,10 @@ total_bill = bill + total_tip_amt
 bill_per_person = total_bill / people
 final_amt = round(bill_per_person, 2)
 print(f"Each persson should pay: ${final_amt}")
+
+#OUTPUT
+Welcome to the tip calculator!
+What was the total bill? $150
+What percentage would you like to give? 10 , 12, or 15? 10
+How many people to split the bill? 5
+Each persson should pay: $33.0
